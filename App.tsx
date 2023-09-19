aceecd1f19ff82b8cd5d330ae5b5213f20a0a69e
Elin Nyman <elin.nyman87@gmail.com>
2023-09-19 15:56:11 +0200

    Lekt med animationer

--- App.tsx
+++ App.tsx
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useEffect, useRef } from "react";
+import { Animated, Easing, Image, StyleSheet } from "react-native";
 
 export default function App() {
+  const scaleValue = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    animatePulsate();
+  }, []);
+
+  const animatePulsate = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(scaleValue, {
+          toValue: 1.1,
+          duration: 1000,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: false,
+        }),
+        Animated.timing(scaleValue, {
+          toValue: 1,
+          duration: 1000,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: false,
+        }),
+      ])
+    ).start();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <LinearGradient
+        colors={["#4c669f", "#3b5998", "#192f6a"]} //blå
+        // colors={["#efc275", "#E9AE49", "#DB941A"]} //gul
+        style={styles.background}
+      >
+        <Animated.View
+          style={[styles.imgContainer, { transform: [{ scale: scaleValue }] }]}
+        >
+          <Image
+            source={require(`../expo_demo/assets/pngwing.png`)}
+            style={styles.img}
+          />
+        </Animated.View>
+      </LinearGradient>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  imgContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    borderRadius: 200,
+    backgroundColor: "#4365ac", //blå
+    // backgroundColor: "#E49C22", //gul
+  },
+  img: {
+    justifyContent: "center",
+    height: 200,
+    width: 200,
+    transform: [{ scaleX: -1 }],
+  },
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
